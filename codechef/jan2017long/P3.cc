70d2c0cc7a13993db7103bc1063bc852272eada4
Jatin Khurana <jatinkhurana9@gmail.com>
2017-01-16 12:04:27 +0530

    Codechef Jan 2017

--- codechef/jan2017long/P3.cc
+++ codechef/jan2017long/P3.cc
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<string>
+#include<iostream>
 
 using namespace std;
 
@@ -19,7 +20,7 @@ int getValue(char c)
 }
 
 // In case of brick,only porblem is at the bottom of the brick
-int brickcheck(int i,int j,int n,int m,int arr[][])
+int brickcheck(int i,int j,int n,int m,int arr[][1000])
 {
 	if(i==n-1)
 		return 0;
@@ -30,7 +31,7 @@ int brickcheck(int i,int j,int n,int m,int arr[][])
 
 }
 
-int watercheck(int i,int j,int n,int m,int arr[][])
+int watercheck(int i,int j,int n,int m,int arr[][1000])
 {
 	// check left 
 	if(j==0)
@@ -50,23 +51,31 @@ int watercheck(int i,int j,int n,int m,int arr[][])
 	return 0;
 }
 
-int aircheck(int i,int j,int n,int m,int arr[][])
+int aircheck(int i,int j,int n,int m,int arr[][1000])
 {
-	// check left
-	if(j==0)
-		return 0;
-	if(arr[i][j-1])
+	//check left
+	if(j-1>=0 && arr[i][j-1]==3)
+		return 1;
+	// check right
+	if(j+1<m && arr[i][j+1]==3)
+		return 1;
+	// check  top
+	if(i-1>=0 && arr[i-1][j]!=1)
+		return 1;
+	return 0;
 	
 }
 
 int main()
 {
 	int t,n,m;
+	bool flag=false;
 	int arr[1000][1000];
 	scanf("%d",&t);
 	string s;
 	while(t--)
 	{
+		flag = false;
 		scanf("%d%d",&n,&m);
 		for(int i=0;i<n;i++)
 		{
@@ -80,20 +89,38 @@ int main()
 		{
 			for(int j=0;j<m;j++)
 			{
-				if(arr[i][j]==2)
+				if(arr[i][j]==1)
 				{
 					if(aircheck(i,j,n,m,arr))
+					{
+						flag = true;
+						break;
+					}
 				}
-				else if(arr[i][j]==3)
+				else if(arr[i][j]==2)
 				{
-
+					if(brickcheck(i,j,n,m,arr))
+					{
+						flag=true;
+						break;
+					}
 				}
 				else
 				{
-
+					if(watercheck(i,j,n,m,arr))
+					{
+						flag = true;
+						break;
+					}
 				}
 			}
+			if(flag)
+				break;
 		}
+		if(flag)
+			printf("no\n");
+		else
+			printf("yes\n");
 	}
 
 	return 0;
